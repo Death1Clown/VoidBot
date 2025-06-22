57b8886a6d096e8536688db22fb5c450e602cedf
Death1clown <goviner9723@gmail.com>
2025-06-22 08:31:56 +0100

    Update password.js

--- admin/admin/secret/token/password.js
+++ admin/admin/secret/token/password.js
@@ -1,8 +1,1 @@
-window-password {"admin1access"}
-window-function {"logadminaccess"}
-function logadminaccess {
-  _set {"admin-access"} = {"client-token"} + {text:"-"} + {"timelaps-runpassword"}
-}
-function runpassword {
-  if {"window-password"} {text:"admin1access"}
-}
+
